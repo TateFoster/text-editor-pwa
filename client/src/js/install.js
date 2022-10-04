f6cb9b85c02b09f653d7e376e9c60114417e0700
Dragon Lore <tate.j.foster@gmail.com>
2022-10-04 15:49:11 -0600

    changing install event handlers

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,24 +3,25 @@ const butInstall = document.getElementById("buttonInstall");
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
-	event.preventDefault();
+	window.deferredPrompt = event;
 	butInstall.style.visibility = "visible";
-
-	butInstall.addEventListener("click", async () => {
-		const promptEvent = window.deferredPrompt;
-
-		if (!promptEvent) {
-			return;
-		}
-		event.prompt();
-		butInstall.setAttribute("disabled", true);
-		butInstall.textContent = "Installed";
-	});
-
-	// TODO: Implement a click event handler on the `butInstall` element
 });
+butInstall.addEventListener("click", async () => {
+	const promptEvent = window.deferredPrompt;
+
+	if (!promptEvent) {
+		return;
+	}
+	promptEvent.prompt();
+
+	window.deferredPrompt = null;
+	butInstall.setAttribute("disabled", true);
+	butInstall.textContent = "Installed";
+});
+
+// TODO: Implement a click event handler on the `butInstall` element
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
-	console.log("app installed", event);
+	window.deferredPrompt = null;
 });
